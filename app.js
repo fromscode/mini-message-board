8f4f2943dbbb17ac557b02d9b7528e7fc644a6a7
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-15 14:04:14 +0000

    Setup routes and middleware error handlers

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const Express = require("express");
+const indexRouter = require("./routes/index");
+const newRouter = require("./routes/new");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = Express();
 
-app.use("/", (req, res) => {
-    res.send("Hello world");
-});
+app.use("/", indexRouter);
+app.use("/new", newRouter);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(3000, (err) => {
     if (err) {
