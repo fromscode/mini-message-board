ed918e278bdd3e7ec4a0acdd84aec5dab4eff5ac
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-16 11:05:33 +0000

    Display static messages at home route

--- router/index.js
+++ router/index.js
@@ -1,8 +1,21 @@
 const Express = require("express");
 const router = Express.Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { messages: messages });
 });
 
 module.exports = router;
