35837f09d4acad09d7f566a9cba423815738e69a
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-17 07:22:37 +0000

    Revert previous commmit as there was no error with the codebase
    This reverts commit c22194eb46be377714fbe24442a7d3b5c24dfc2c.

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const router = require("./router/index");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+const path = require("node:path");
+
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server up and running at port ${PORT}`);
+app.listen(PORT, "0.0.0.0", (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log(`Server up and running at port ${PORT}`);
+    }
 });
