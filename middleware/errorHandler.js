1f3596770d171a2141fe2aca7f48613574c39c62
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-18 05:10:02 +0000

    Minor fixes

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,9 @@
 module.exports = (error, req, res, next) => {
     console.error(error);
 
-    res.status(500).render("error", { err: error });
+    res.status(500).render("error", {
+        err: {
+            message: "Internal Server Error",
+        },
+    });
 };
